80e48060d21777b1cbcd0492eee11850de1af3d4
Alexander Stehlik <alexander.stehlik@gmail.com>
2016-07-28 11:21:15 +0200

    [TASK] Initial version based on 2.3.0

---
+++ ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "naw_single_signon".
+ *
+ * Auto generated | Identifier: 434fbf93c21e379b78247e85ca49aca6
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Single Sign-On',
+	'description' => 'The TYPO3 Single Sign-On (SSO) extension provides seamless integration of third-party (i.e. non-TYPO3) applications (TPAs) into TYPO3. This includes end-user access to TPAs with no additional logon.',
+	'category' => 'plugin',
+	'version' => '2.3.0',
+	'state' => 'stable',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Dietrich Heise',
+	'author_email' => 'typo3-ext@naw.info',
+	'author_company' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.5.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+	'_md5_values_when_last_written' => 'a:31:{s:40:"class.tx_nawsinglesignon_usermapping.php";s:4:"f01b";s:21:"ext_conf_template.txt";s:4:"9d48";s:12:"ext_icon.gif";s:4:"f924";s:17:"ext_localconf.php";s:4:"bb1c";s:14:"ext_tables.php";s:4:"5d51";s:14:"ext_tables.sql";s:4:"a1e2";s:15:"flexform_ds.xml";s:4:"751c";s:13:"locallang.xml";s:4:"9800";s:33:"locallang_csh_nawsinglesignon.xml";s:4:"a693";s:27:"locallang_csh_ttcontent.xml";s:4:"a693";s:16:"locallang_db.xml";s:4:"7458";s:17:"locallang_tca.xml";s:4:"7481";s:14:"doc/manual.sxw";s:4:"b390";s:12:"doc/todo.sxw";s:4:"7aef";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"3277";s:14:"mod1/index.php";s:4:"c630";s:18:"mod1/locallang.xml";s:4:"2330";s:22:"mod1/locallang_mod.xml";s:4:"033b";s:25:"mod1/mapping_help1_en.png";s:4:"e4e1";s:25:"mod1/mapping_help2_en.png";s:4:"f8e6";s:25:"mod1/mapping_help3_en.png";s:4:"0978";s:24:"mod1/mapping_help_en.gif";s:4:"c3f7";s:22:"mod1/single-signon.css";s:4:"8a9f";s:19:"mod1/top_header.gif";s:4:"bde7";s:20:"mod1/usermapping.gif";s:4:"3161";s:14:"pi1/ce_wiz.gif";s:4:"7060";s:36:"pi1/class.tx_nawsinglesignon_pi1.php";s:4:"e118";s:44:"pi1/class.tx_nawsinglesignon_pi1_wizicon.php";s:4:"3c66";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"f6ae";}',
+);
+
+?>
